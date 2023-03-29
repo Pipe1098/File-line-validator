71e01a9899c64e5d6c4f186a69db86625106e517
Luis Felipe Penagos <pipe1e@hotmail.com>
2023-03-29 11:58:52 -0500

    Renombrando y cambiando puerto

--- Processor-Service/src/main/java/com/example/taller/procesador/archivo/Service/FileProcessor.java
+++ Processor-Service/src/main/java/com/example/taller/procesador/archivo/Service/FileProcessor.java
@@ -3,7 +3,6 @@ package com.example.taller.procesador.archivo.Service;
 import com.example.taller.procesador.archivo.Model.Data;
 import com.example.taller.procesador.archivo.Model.Persona;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpHeaders;
@@ -15,18 +14,18 @@ public class FileProcessor {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        String url = "http://localhost:8082/ArchivosCSV";
+        String url = "http://localhost:8090/ArchivosCSV";
         HttpEntity<Persona> requestPersona = new HttpEntity<>(persona,headers);
         boolean respuesta = restTemplate.postForObject(url,requestPersona, boolean.class);
 
         return respuesta;
     }
-    public static boolean pasarValidarXML(Data data) {
+    public static boolean pasarValidarXLS(Data data) {
 
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        String url = "http://localhost:8082/ArchivosXLS";
+        String url = "http://localhost:8090/ArchivosXLS";
         HttpEntity<Data> requestData = new HttpEntity<>(data,headers);
         boolean respuesta = restTemplate.postForObject(url,requestData, boolean.class);
 
