0c56278deb212ffb7ddbced79e53cb3ed34f6a77
Luis Felipe Penagos <pipe1e@hotmail.com>
2023-03-29 12:00:22 -0500

    Update FilesReader.java

--- Processor-Service/src/main/java/com/example/taller/procesador/archivo/Service/FilesReader.java
+++ Processor-Service/src/main/java/com/example/taller/procesador/archivo/Service/FilesReader.java
@@ -1,14 +1,7 @@
 package com.example.taller.procesador.archivo.Service;
 
-import com.example.taller.procesador.archivo.Model.Data;
-import com.example.taller.procesador.archivo.Model.Persona;
-
-import java.util.List;
-import java.util.Objects;
-
 public interface FilesReader {
 
     public Object read(String path);
-
-
+    
 }
